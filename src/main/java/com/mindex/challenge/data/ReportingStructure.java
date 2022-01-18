c57ba7982daf8b46f225eedd11ac75052dbd0934
Nick Kowalczuk <pnk8000@rit.edu>
2022-01-17 19:51:12 -0500

    added naive reporting structure class to data file

--- src/main/java/com/mindex/challenge/data/ReportingStructure.java
+++ src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -1,5 +1,18 @@
 package com.mindex.challenge.data;
 
 public class ReportingStructure {
-    
+    private String employeeId;
+    private int directReports;
+
+    public ReportingStructure(String id){
+        this.employeeId = id;
+        this.directReports = 0;
+    }
+
+    public String getId(){
+        return employeeId;
+    }
+    public int getDirectReports(){
+        return directReports;
+    }
 }
