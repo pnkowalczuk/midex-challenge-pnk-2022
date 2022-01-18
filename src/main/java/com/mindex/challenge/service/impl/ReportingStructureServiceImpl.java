3ff7b688f118c9a398d7158b3cdff76e118d4fa2
Nick Kowalczuk <pnk8000@rit.edu>
2022-01-17 20:05:21 -0500

    added rs service and impl

---
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -0,0 +1,15 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.ReportingStructureService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportingStructureServiceImpl implements ReportingStructureService{
+    
+
+    public ReportingStructure compute(String id){
+        ReportingStructure rs = new ReportingStructure("noid");
+        return rs;
+    }
+}
