a3dd1fce15a81cfa33177e7a84ab81ff483e68f0
Nick Kowalczuk <pnk8000@rit.edu>
2022-01-19 20:52:15 -0500

    changed function to compile a list of employees instead of counting

--- src/main/java/com/mindex/challenge/data/ReportingStructure.java
+++ src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -2,6 +2,9 @@ package com.mindex.challenge.data;
 
 
 import java.util.List;
+import java.util.Objects;
+import java.util.HashSet;
+import java.util.ArrayList;
 
 import com.mindex.challenge.dao.EmployeeRepository;
 // import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +13,27 @@ public class ReportingStructure {
     private String employeeId;
     private int directReports;
 
+
     public ReportingStructure(String id, EmployeeRepository employeeRepository){
         this.employeeId = id;
         List<Employee> reportingEmployees = employeeRepository.findByEmployeeId(id).getDirectReports();
-        directReports = reportingEmployees.size();
+        directReports = getTotalReports(reportingEmployees, employeeRepository);
 
+
+    }
+
+
+    //TODO change this to prevent duplicated persons.
+    public int getTotalReports(List<Employee> directReportsList, EmployeeRepository employeeRepository){
+        for(int i = 0; i < directReportsList.size(); i++){
+            
+            Employee curEmp = employeeRepository.findByEmployeeId(directReportsList.get(i).getEmployeeId());
+            List<Employee> curEmpReports = curEmp.getDirectReports();
+            if(!Objects.isNull(curEmpReports) && curEmpReports.size() > 0){
+                directReportsList.addAll(curEmpReports);
+            }
+        }
+        return directReportsList.size();
     }
 
     public String getId(){
